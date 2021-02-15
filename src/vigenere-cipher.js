aaf929e242f53899b19713d8308de775ae8bc54f
Grimmbergen <vialyur@list.ru>
2021-02-15 23:56:15 +0300

    add: all solutions (including Vigenere Cipher)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,8 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {}
-  decrypt() {}
+  constructor(notReverse = true) {
+    this.notReverse = notReverse;
+    this.alphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  }
+
+  encrypt(text, key) {
+    if (!text || !key) throw new Error();
+    let textArr = [...text.toUpperCase()];
+    let keyArr = [...key.toUpperCase()];
+    let indKey = 0;
+    let res = textArr.map((symb) => {
+      if (this.alphabet.includes(symb)) {
+        let symbRes = this.alphabet[
+          (this.alphabet.indexOf(symb) +
+            this.alphabet.indexOf(keyArr[indKey % keyArr.length])) %
+            this.alphabet.length
+        ];
+        indKey++;
+        return symbRes;
+      } else {
+        return symb;
+      }
+    });
+    return this.notReverse ? res.join("") : res.reverse().join("");
+  }
+
+  decrypt(text, key) {
+    if (!text || !key) throw new Error();
+    let textArr = [...text.toUpperCase()];
+    let keyArr = [...key.toUpperCase()];
+    let indKey = 0;
+    let res = textArr.map((symb) => {
+      if (this.alphabet.includes(symb)) {
+        let symbRes = this.alphabet[
+          (this.alphabet.indexOf(symb) -
+            this.alphabet.indexOf(keyArr[indKey % keyArr.length]) +
+            this.alphabet.length) %
+            this.alphabet.length
+        ];
+        indKey++;
+        return symbRes;
+      } else {
+        return symb;
+      }
+    });
+    return this.notReverse ? res.join("") : res.reverse().join("");
+  }
 }
 
 module.exports = VigenereCipheringMachine;
